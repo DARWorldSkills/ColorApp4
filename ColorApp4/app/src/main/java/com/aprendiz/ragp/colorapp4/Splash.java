101db00457471d827e7594ba3797fd4b9bc0cbca
DavidPyn27 <popayan159@gmail.com>
2018-08-20 12:40:58 -0500

    diseño implementado

--- ColorApp4/app/src/main/java/com/aprendiz/ragp/colorapp4/Splash.java
+++ ColorApp4/app/src/main/java/com/aprendiz/ragp/colorapp4/Splash.java
@@ -3,16 +3,35 @@ package com.aprendiz.ragp.colorapp4;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
 
+    ImageView ImgSplash;
+    ViewFlipper viewFlipper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImgSplash = findViewById(R.id.ImgSplash);
+        viewFlipper = findViewById(R.id.view);
+
+        Animation in = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        Animation our = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+
+        viewFlipper.setInAnimation(in);
+        viewFlipper.setOutAnimation(our);
+
+        viewFlipper.showNext();
+
+
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
