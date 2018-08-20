b8394240ecb5648883cb7bc9be84330d0410251b
Rodrigo Alejandro Guspian Perez <raguspian@misena.edu.co>
2018-08-20 12:57:59 -0500

    commitR21

--- ColorApp4/app/src/main/java/com/aprendiz/ragp/colorapp4/MenuT.java
+++ ColorApp4/app/src/main/java/com/aprendiz/ragp/colorapp4/MenuT.java
@@ -1,13 +1,33 @@
 package com.aprendiz.ragp.colorapp4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuT extends AppCompatActivity {
-
+    public static int guardar=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_t);
+    }
+
+    public void jugar(View view) {
+        guardar=1;
+        Intent intent = new Intent(MenuT.this,Juego.class);
+        startActivity(intent);
+    }
+
+    public void puntuacion(View view) {
+        guardar=0;
+        Intent intent = new Intent(MenuT.this,Juego.class);
+        startActivity(intent);
+    }
+
+    public void configuracion(View view) {
+        guardar=0;
+        Intent intent = new Intent(MenuT.this,Configuracion.class);
+        startActivity(intent);
     }
 }
